6a1150ac83f10244fa633536d894ade51a6ad679
CptFuad <mdfatinfuad56@gmail.com>
2026-03-18 15:39:36 +0600

    Update app

--- app/app.py
+++ app/app.py
@@ -2,16 +2,27 @@ import streamlit as st
 import joblib
 import numpy as np
 
-model= joblib.load('models/model.pkl')
+model= joblib.load('../models/model.pkl')
 
 st.title('Heart Disease Predication System')
 
 age=st.number_input('Age')
+sex=st.number_input('Sex')
+cp=st.number_input('Chest Pain Type')
+trestbps=st.number_input('Resting Blood Pressure')
 chol=st.number_input('Cholestrol')
-bp=st.number_input('Blood Pressure')
+fbs=st.number_input('Fasting Blood Sugar')
+restecg=st.number_input('Resting Electrocardiographic Results (ECG)')
+thalach=st.number_input('Maximum Heart Rate Achieved')
+exang=st.number_input('Exercise Induced Angina')
+oldpeak=st.number_input('ST Depression induced by exercise relative to rest')
+slope=st.number_input('Slope of the Peak Exercise ST Segment')
+ca=st.number_input('Number of Major Vessels')
+thal=st.number_input('Thalassemia')
+
 
 if st.button('Predict'):
-    data=np.array([[age, chol, bp]])
+    data=np.array([[age,sex,cp,trestbps,chol, fbs, restecg, thalach,exang,oldpeak, slope, ca,thal]])
     result=model.predict(data)
 
     if result[0]==1:
